e43f2ae0d8042db53c2b1f94d3301bcaa700f3da
Steve M <s.mander3@lancaster.ac.uk>
2025-02-03 23:54:04 +0000

    update reflector constructor

--- DataModule.py
+++ DataModule.py
@@ -18,11 +18,29 @@ class EnigmaDataModule(pl.LightningDataModule):
             
             self.rotors=[rotorA,rotorB,rotorC]
         if reflector is None:
-            reflector=torch.randperm(26)
-            #The rule of the reflector is that no letter can map to itself.
-            while (reflector==torch.arange(26)).sum()>0:
-                reflector=torch.randperm(26)
-            self.reflector=reflector
+            self.reflector=torch.arange(26)
+            while torch.any(self.reflector-torch.arange(26)==0):
+                #swap 2 random elements on the diagonal of the reflector
+                indexes_to_swap=torch.arange(26)-self.reflector ==0
+                indexes_to_swap=indexes_to_swap.to(torch.long)
+                #swap the elements
+                #first location is the first true in the indexes to swap
+                firstLocation=torch.argmax(indexes_to_swap)
+                #second location is a random int in indexes to swap that is not the first location
+                secondLocations=torch.randperm(26)
+                index=0
+                while (secondLocations[index]==firstLocation or not indexes_to_swap[secondLocations[index]]):
+                    index+=1
+                    #print(secondLocation)
+                #swap the elements
+                a=self.reflector[firstLocation].item()
+                b=self.reflector[secondLocations[index]].item()
+                self.reflector[secondLocations[index]]=a
+                self.reflector[firstLocation]=b
+
+            #check that the reflector has all numbers 1 to 26
+            assert torch.sum(torch.pow(self.reflector,2))==torch.sum(torch.pow(torch.arange(26),2))
+            
         #These constitute the GROUND Truth Enigma settings. 
         self.dataset=EnigmaDataset(self.rotors,self.reflector)
 
@@ -58,7 +76,17 @@ class EnigmaDataset(torch.utils.data.IterableDataset):
         #We will generate a sequence of 50 random letters, then encode it.
 
         GROUND_TRUTH= torch.randint(0,26,(11*26,),dtype=torch.long)
-        encoded=GROUND_TRUTH.clone()
+        encoded=self.ENCODEGroudTruth(GROUND_TRUTH)
+
+        ## To Experiment with: 
+        # -  Add noise to the encoded message? Try randomizing a character in the encoded message, to represent a mistake in the encoding.
+        # -  Try doing the above step with linear algebra, to make it faster and remove the for loop and the if statements.
+        # -        -- HINT: You may want to think of the rotors as 1-hot encoded matrices of shape 26,26 and the encoding as a matrix multiplication. 
+              
+        
+        return encoded,GROUND_TRUTH
+    def ENCODEGroudTruth(self,GROUND_TRUTH):
+        encoded=torch.clone(GROUND_TRUTH)
         rotor_positions=[0,0,0]
         for i in range(11*26):
             #Rotate the rotors
@@ -71,25 +99,32 @@ class EnigmaDataset(torch.utils.data.IterableDataset):
             letterAfterThirdRotor= self.rotors[2][(rotor_positions[2] + letterAfterSecondRotor)%26]
             reflectedLetter=self.reflector[letterAfterThirdRotor]
             #Now we have to go back through the rotors
-            letterReturningThroughThirdRotor=self.rotors[2][(rotor_positions[2]+reflectedLetter)%26]
-            letterReturningThroughSecondRotor=self.rotors[1][(rotor_positions[1]+letterReturningThroughThirdRotor)%26]
-            letterReturningThroughFirstRotor=self.rotors[0][(rotor_positions[0]+letterReturningThroughSecondRotor)%26]
+            #meaning we find the index of the number, 
+            
+            letterReturningThroughThirdRotor=(self.rotors[2][rotor_positions[2]:].tolist() + self.rotors[2][:rotor_positions[2]].tolist()).index(reflectedLetter)
+            letterReturningThroughSecondRotor=(self.rotors[1][rotor_positions[1]:].tolist() + self.rotors[1][:rotor_positions[1]].tolist()).index(letterReturningThroughThirdRotor)
+            letterReturningThroughFirstRotor=(self.rotors[0][rotor_positions[0]:].tolist() + self.rotors[0][:rotor_positions[0]].tolist()).index(letterReturningThroughSecondRotor)
             #This is the encoded letter
             encoded[i]=letterReturningThroughFirstRotor
+        return encoded
 
-        ## To Experiment with: 
-        # -  Add noise to the encoded message? Try randomizing a character in the encoded message, to represent a mistake in the encoding.
-        # -  Try doing the above step with linear algebra, to make it faster and remove the for loop and the if statements.
-        # -        -- HINT: You may want to think of the rotors as 1-hot encoded matrices of shape 26,26 and the encoding as a matrix multiplication. 
-              
-        
-        return encoded,GROUND_TRUTH
-    
+
 if __name__=="__main__":
+    import matplotlib.pyplot as plt
     dm=EnigmaDataModule()
     dl=dm.train_dataloader()
     for i in dl:
-        print(i)
+        fig= plt.figure()
+        encoded,ground_truth=i
+        ax1=fig.add_subplot(121)
+        ax2=fig.add_subplot(122)
+        ax1.imshow(encoded)
+        ax2.imshow(ground_truth)
+        plt.show()
         break
-    print("done")
-    #print(dl)
+    print("Checking Properties")
+    GROUND_TRUTH= torch.randint(0,26,(11*26,),dtype=torch.long)
+    encoded=dm.dataset.ENCODEGroudTruth(GROUND_TRUTH)
+
+    assert torch.allclose(dm.dataset.ENCODEGroudTruth(encoded),GROUND_TRUTH)
+    print("All tests passed")
