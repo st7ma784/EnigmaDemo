ba60519cc89abea0726b50e125f11845c36aed61
STEVE <s.mander3@lancaster.ac.uk>
2025-02-05 11:56:14 +0000

    ffx

--- pytorchGSTest.py
+++ pytorchGSTest.py
@@ -36,65 +36,65 @@ def matching(alpha):
     permutation_matrix = coo_matrix((np.ones_like(row), (row, col))).toarray()
     return torch.from_numpy(permutation_matrix)
 
+if __name__== "__main__":
+    num_steps = 100
+    res=0
+    CEloss=torch.nn.CrossEntropyLoss()
+    L1=torch.nn.L1Loss()
+    BinaryCEloss=torch.nn.BCELoss()
 
-num_steps = 100
-res=0
-CEloss=torch.nn.CrossEntropyLoss()
-L1=torch.nn.L1Loss()
-BinaryCEloss=torch.nn.BCELoss()
+    # Test Params
+    LRs=[0.1, 0.2, 0.5, 0.7, ]
+    noises=[i/5 for i in range(5)]
+    taus=[0.01, 0.03, 0.05, 0.1, 0.5]
+    n_iters=[1,10,  50, 100, 500]
+    scale=[1,10,1000,10000]
+    Loss_methods=["CEloss", "L1", ]
+    results=torch.zeros(len(LRs), len(noises), len(taus), len(n_iters),len(scale), len(Loss_methods))
+    labels=torch.nn.functional.one_hot(torch.arange(26)).float()
+    for LRi in range(len(LRs)):
+        lr=LRs[LRi]
+        for noise in range(len(noises)):
+            noise_factor=noises[noise]
+            for tauI in range(len(taus)):
+                tau=taus[tauI]
+                for n_iterI in range(len(n_iters)):
+                    n_iter=n_iters[n_iterI]
+                    for scaleI in range(len(scale)):
+                        scaleFactor = scale[scaleI]
+                        for loss_methodI in range(len(Loss_methods)):
+                            lossMethod=Loss_methods[loss_methodI]
+                            if lossMethod=="CEloss":
+                                lossCall=CEloss
+                            elif lossMethod=="L1":
+                                lossCall=L1
+                            else:
+                                lossCall=BinaryCEloss
+                            for i in range(3):
+                                losses=[]
+                                log_alpha = torch.randn(26, 26)
+                                log_alpha.requires_grad = True
+                                loss=lossCall(log_alpha, labels)
+                                for test in tqdm(range(num_steps)):
+                                    WithGradtensor = gumbel_sinkhorn(scaleFactor*log_alpha, tau=tau, n_iter=n_iter, noise_factor=noise_factor)
+                                    Loss=lossCall(WithGradtensor, labels)
+                                    Loss.backward()
+                                    #apply gradient descent
+                                    log_alpha.data -= lr * log_alpha.grad
+                                    log_alpha.grad.zero_()
 
-# Test Params
-LRs=[0.1, 0.2, 0.5, 0.7, ]
-noises=[i/5 for i in range(5)]
-taus=[0.01, 0.03, 0.05, 0.1, 0.5]
-n_iters=[1,10,  50, 100, 500]
-scale=[1,10,1000,10000]
-Loss_methods=["CEloss", "L1", ]
-results=torch.zeros(len(LRs), len(noises), len(taus), len(n_iters),len(scale), len(Loss_methods))
-labels=torch.nn.functional.one_hot(torch.arange(26)).float()
-for LRi in range(len(LRs)):
-    lr=LRs[LRi]
-    for noise in range(len(noises)):
-        noise_factor=noises[noise]
-        for tauI in range(len(taus)):
-            tau=taus[tauI]
-            for n_iterI in range(len(n_iters)):
-                n_iter=n_iters[n_iterI]
-                for scaleI in range(len(scale)):
-                    scaleFactor = scale[scaleI]
-                    for loss_methodI in range(len(Loss_methods)):
-                        lossMethod=Loss_methods[loss_methodI]
-                        if lossMethod=="CEloss":
-                            lossCall=CEloss
-                        elif lossMethod=="L1":
-                            lossCall=L1
-                        else:
-                            lossCall=BinaryCEloss
-                        for i in range(3):
-                            losses=[]
-                            log_alpha = torch.randn(26, 26)
-                            log_alpha.requires_grad = True
-                            loss=lossCall(log_alpha, labels)
-                            for test in tqdm(range(num_steps)):
-                                WithGradtensor = gumbel_sinkhorn(scaleFactor*log_alpha, tau=tau, n_iter=n_iter, noise_factor=noise_factor)
-                                Loss=lossCall(WithGradtensor, labels)
-                                Loss.backward()
-                                #apply gradient descent
-                                log_alpha.data -= lr * log_alpha.grad
-                                log_alpha.grad.zero_()
+                                print("start Loss : {} -> end Loss : {}".format(loss, Loss))
+                                losses.append(Loss)
 
-                            print("start Loss : {} -> end Loss : {}".format(loss, Loss))
-                            losses.append(Loss)
+                    results[LRi, noise, tauI, n_iterI,scaleI,loss_methodI]=sum(losses)/len(losses)
+    #find the smallest loss
+    minLoss=torch.min(results)
+    print(minLoss)
+    #find the x,y,z,w,v,u values of the minloss location in the array
+    minLossLocation=torch.argmin(results.flatten(0,-1))
 
-                results[LRi, noise, tauI, n_iterI,scaleI,loss_methodI]=sum(losses)/len(losses)
-#find the smallest loss
-minLoss=torch.min(results)
-print(minLoss)
-#find the x,y,z,w,v,u values of the minloss location in the array
-minLossLocation=torch.argmin(results.flatten(0,-1))
-
-coords=torch.unravel_index(minLossLocation, results.shape)
-best_settings=[LRs[coords[0]], noises[coords[1]], taus[coords[2]], n_iters[coords[3]], scale[coords[4]], Loss_methods[coords[5]]]
-print(best_settings)
+    coords=torch.unravel_index(minLossLocation, results.shape)
+    best_settings=[LRs[coords[0]], noises[coords[1]], taus[coords[2]], n_iters[coords[3]], scale[coords[4]], Loss_methods[coords[5]]]
+    print(best_settings)
 
 
